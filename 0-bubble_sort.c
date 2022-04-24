4e40f8da29b77bc2d0e107485e27e89f98e3687f
pericles001 <periclesadjovi@outlook.fr>
2022-04-24 19:31:13 +0100

    Remove swap_ints method

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,6 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
-void swap_ints(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
  * bubble_sort - Sort an array of integers in ascending order.
  * @array: An array of integers to sort.
  * @size: The size of the array.
