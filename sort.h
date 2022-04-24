a408f25d170078c2399ba7eff629003d88fd3a99
pericles001 <periclesadjovi@outlook.fr>
2022-04-24 19:10:47 +0100

    Added enum bool and macros

--- sort.h
+++ sort.h
@@ -4,6 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Comparison direction macros for bitonic sort*/
+#define UP 0
+#define DOWN 1
+
+/*enum bool - Enumeration of boolean values*/
+typedef enum bool
+{
+    false = 0;
+    true
+} bool;
+
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,9 +30,18 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
+/*Helper for swapping*/
+void swap_ints(int *a, int *b);
+
 /*Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/*Regular functions prototypes*/
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 
